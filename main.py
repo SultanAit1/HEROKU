aeb35d2991628bf729b4f9fc2cd5103877abeb19
aitmanbetsul514@gmail.com <aitmanbetsul514@gmail.com>
2023-03-31 17:13:14 +0600

    af

--- main.py
+++ main.py
@@ -4,16 +4,12 @@ from aiogram.utils import executor
 
 from config import dp
 from handlers import client
-from aiogram import Bot, Dispatcher
+
 
 client.register_handlers_client(dp)
 
 
-TOKEN='5651777397:AAHQ_SX6AYfnzfbDQvA5iK_VxCbc4o9gmp0'
 
-bot = Bot()
-dp = Dispatcher(bot=bot)
-ADMIN_ID = [661114436,]
 
 
 
