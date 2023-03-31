7a50ec89e6c2c60c893f378446d818e5311d48e9
aitmanbetsul514@gmail.com <aitmanbetsul514@gmail.com>
2023-03-31 16:24:55 +0600

    af

--- config.py
+++ config.py
@@ -2,10 +2,9 @@ from aiogram import Bot, Dispatcher
 from  decouple import config
 
 
-
+TOKEN='5651777397:AAHQ_SX6AYfnzfbDQvA5iK_VxCbc4o9gmp0'
 TOKEN = config('TOKEN')
 
 bot = Bot(TOKEN)
 dp = Dispatcher(bot=bot)
 ADMIN_ID = [661114436,]
-bot.run(config('TOKEN'))
--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from aiogram.utils import executor
 
 from config import dp
 from handlers import client
-
+from decouple import config
 client.register_handlers_client(dp)
 
-
+bot = executor.Bot(token=config('TOKEN'))
 
 
 
